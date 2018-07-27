6390e7022e82e967e0334e0d596da40fc031ccee
Florian Turowski <florian.turowski@vwfs.com>
2018-07-27 15:00:29 +0200

    add method setNewStatus

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app           = express();
 const fs            = require('fs');
 const bodyParser    = require('body-parser');
 
-const PORT      = 8081;
-const DATAFILE  = './data/cards.json';
-
+const //application consts
+ PORT      = 8081,
+ DATAFILE  = './data/cards.json';
+//global model
 var cards;
 
 fs.readFile(DATAFILE,'utf-8',function(err,content){
@@ -15,6 +16,7 @@ fs.readFile(DATAFILE,'utf-8',function(err,content){
         cards =  JSON.parse(content);
     }
 });
+
 
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
@@ -31,23 +33,16 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+
+
+/*
+ * API functions
+ */
 app.get('/getCards',function(req,res){
     res.send(JSON.stringify(cards));
     res.status(200).end('Cards sent');
 });
 app.post('/addCard',function(req,res,next){
-    /*
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString(); // convert Buffer to string
-    });
-    req.on('end', () => {
-        console.log(body);
-        res.end('ok');
-    });
-    */
-    //return;
-
     if (req.body.text !== undefined){
         let card = { id      : Date.now(),text    : req.body.text};
         cards.backlog.items.push(card);
@@ -57,9 +52,57 @@ app.post('/addCard',function(req,res,next){
         next("Parameter missing");
     }
 
+    saveCards(cards);
 });
+app.post( '/setNewStatus',function(req,res){
+    var 
+    elementFound    = null,
+    data            = cards,
+    itemId          = req.body.itemId,
+    stop            = false;
 
+    console.log(`Item ${itemId} clicked`);
+
+    for (var section in data){
+        console.log(section);
+        if (!stop){
+            if(elementFound === null){
+                data[section].items = data[section].items.map(element => {
+                    if(itemId === element.id){
+                        elementFound = element;
+                        return null;
+                    }else{
+                        return element;
+                    }
+                }).filter(nullElement => {
+                    if(nullElement !== null){
+                        return nullElement;
+                    }
+                });
+            }else{
+                data[section].items.push(elementFound);
+                elementFound = null;
+                stop = true;
+            }
+        }
+    }
+
+    res.status(200).end(JSON.stringify(cards));
+
+    saveCards(cards);
+});
+/*
+ * Starting server
+ */
 const server = app.listen(PORT,function(){
     console.log(`Server is listening at http:${server.address().address}:${PORT}`);
 });
+
+const saveCards = function(updatedCards){
+    fs.writeFile( DATAFILE, JSON.stringify(updatedCards), { encoding : 'utf-8'}, function(err){
+        if ( err ) {
+            console.log(err);
+        }
+    });
+};
 
